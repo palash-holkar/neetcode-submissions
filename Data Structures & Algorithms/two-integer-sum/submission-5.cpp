8835b77f57d6fae9bc37e6ce8c08fbbfb85462e5
Palash Holkar <palash.holkar@hitachirail.com>
2026-06-22 20:00:49 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int>mp;
+        //vector<int>ret_v;
+
+        for(int i=0;i<nums.size();++i)
+        {
+            if(mp.find(target-nums[i]) != mp.end())
+            {
+                // ret_v.push_back(mp[target-nums[i]]);
+                // ret_v.push_back(i);
+                return {mp[target-nums[i]],i};
+            }
+            mp.insert({nums[i],i});
+        }
+        return {};
+    }
+};
